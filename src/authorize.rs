f4e0a17a4f5c4f404d24021b27232824f2c96950
Steven Chang <steven.chang@btn.group>
2023-04-25 10:14:26 +1000

    #validate_received_token (#45)

--- src/authorize.rs
+++ src/authorize.rs
@@ -1,8 +1,29 @@
-use cosmwasm_std::{HumanAddr, StdError, StdResult};
+use crate::state::{SecretContract, Token};
+use cosmwasm_std::{Env, HumanAddr, StdError, StdResult, Uint128};
 
 pub fn authorize(expected: HumanAddr, received: HumanAddr) -> StdResult<()> {
     if expected != received {
         return Err(StdError::Unauthorized { backtrace: None });
+    }
+
+    Ok(())
+}
+
+pub fn validate_received_token(token: Token, amount: Uint128, env: &Env) -> StdResult<()> {
+    let token_valid: bool = match token {
+        Token::Snip20(SecretContract {
+            ref address,
+            contract_hash: _,
+        }) => env.message.sender == *address,
+        Token::Native(_) => {
+            env.message.sent_funds.len() == 1 && env.message.sent_funds[0].amount == amount
+        }
+    };
+
+    if !token_valid {
+        return Err(StdError::generic_err(
+            "Received crypto type or amount is wrong.",
+        ));
     }
 
     Ok(())
