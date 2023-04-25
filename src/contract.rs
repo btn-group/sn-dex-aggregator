f3e8a76330fe5bb2a7356d42bc2c843606f5be00
Steven Chang <stevenchang5000@gmail.com>
2023-04-25 21:45:03 +1000

    #hop_messages: send wrapped token to contract

--- src/contract.rs
+++ src/contract.rs
@@ -159,15 +159,9 @@ fn hop_messages(hop: Hop, amount: Uint128, env: &Env) -> StdResult<Vec<CosmosMsg
                 }),
             )?);
             msgs.push(snip20::send_msg(
-                hop.smart_contract.unwrap().address,
+                env.contract.address.clone(),
                 amount,
-                // build swap msg for the next hop
-                Some(to_binary(&Snip20Swap::Swap {
-                    // set expected_return to None because we don't care about slippage mid-route
-                    expected_return: None,
-                    // set the recepient of the swap to be this contract (the router)
-                    to: Some(env.contract.address.clone()),
-                })?),
+                None,
                 None,
                 BLOCK_SIZE,
                 contract_hash,
