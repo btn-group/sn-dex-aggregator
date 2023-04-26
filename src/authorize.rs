75ac122ce8ac787b3387ea589376f6b891dfc493
Steven Chang <stevenchang5000@gmail.com>
2023-04-26 15:13:44 +1000

    #handle_hop: use #validate_received_from_an_allowed_address

--- src/authorize.rs
+++ src/authorize.rs
@@ -1,10 +1,44 @@
-use crate::state::{SecretContract, Token};
+use crate::state::{Hop, SecretContract, Token};
 use cosmwasm_std::{Env, HumanAddr, StdError, StdResult, Uint128};
 
 pub fn authorize(expected: HumanAddr, received: HumanAddr) -> StdResult<()> {
     if expected != received {
         return Err(StdError::Unauthorized { backtrace: None });
     }
+
+    Ok(())
+}
+
+pub fn validate_received_from_an_allowed_address(
+    current_hop: Hop,
+    next_hop: Hop,
+    env: &Env,
+    from: HumanAddr,
+) -> StdResult<()> {
+    match next_hop.from_token {
+        Token::Snip20(SecretContract {
+            address,
+            contract_hash: _,
+        }) => {
+            // Authorize
+            authorize(env.message.sender.clone(), address)?;
+
+            // 1. wrapped (redeem_denom present) - from must be from this contract
+            // 2. from migration contract - from must be from this contract
+            // 3. shade_protocol_router_path - from must be current_hop smart contract
+            if current_hop.redeem_denom.is_some() {
+                authorize(env.contract.address.clone(), from)?;
+            } else if current_hop.migrate_to_token.is_some() {
+                authorize(env.contract.address.clone(), from)?;
+            } else if current_hop.smart_contract.is_some() {
+                authorize(current_hop.smart_contract.unwrap().address, from)?;
+            }
+        }
+        Token::Native(_) => {
+            // Native token in handle_hop can only be from the contract
+            authorize(env.message.sender.clone(), env.contract.address.clone())?;
+        }
+    };
 
     Ok(())
 }
